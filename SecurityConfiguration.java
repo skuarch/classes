6c517acec98aa479faef40e9e5abfffe90b847aa
skuarch <skuarch@yahoo.com.mx>
2015-11-29 13:48:05 -0600

    SecurityConfiguration avoid cache

--- SecurityConfiguration.java
+++ SecurityConfiguration.java
@@ -33,5 +33,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and().csrf().disable().httpBasic();
+		http.headers().cacheControl().disable();
 	}
 }
