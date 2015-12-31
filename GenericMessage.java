df65bb77356bb6480d9293ab5f00ea17f6a9802b
alfredo bello monzoy <skuarch@yahoo.com.mx>
2015-12-31 08:55:21 -0600

    adding new properties

--- GenericMessage.java
+++ GenericMessage.java
@@ -1,62 +1,130 @@
 package model.bean;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
- * this bean is created in order to simplify the controllers.<br/>
- * Bean
- *
+ * Bean.
  * @author skuarch
  */
-public class GenericMessage {
+public class GenericMessage implements Serializable {
 
+    private static final long serialVersionUID = 8555172341255856507L;
     private long id;
     private int code;
     private String message;
+    private long timestamp = System.currentTimeMillis();
+    private String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
-	// =====================================================================
-	/**
-	 * Constructor.
-	 * 
-	 */
-	public GenericMessage() {
-	}
+    //==========================================================================
+    /**
+     * constructor.
+     */
+    public GenericMessage() {
+    }
 
-	// =====================================================================
-	/**
-	 * parameterized constructor.
-	 * 
-	 * @param id long
-	 * @param code int
-	 * @param message String
-	 */
-	public GenericMessage(long id, int code, String message) {
-		this.id = id;
-		this.code = code;
-		this.message = message;
-	}
+    //==========================================================================
+    /**
+     * contrusctor.
+     * @param id long
+     * @param code int
+     * @param message String
+     */
+    public GenericMessage(long id, int code, String message) {
+        this.id = id;
+        this.code = code;
+        this.message = message;
+    }
 
-	// =====================================================================
-	public long getId() {
-		return id;
-	}
+    //==========================================================================
+    /**
+     * getter.
+     * @return id
+     */
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    //==========================================================================
+    /**
+     * setter.
+     * @param id long
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public int getCode() {
-		return code;
-	}
+    //==========================================================================
+    /**
+     * getter.
+     * @return code
+     */
+    public int getCode() {
+        return code;
+    }
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    //==========================================================================
+    /**
+     * setter.
+     * @param code int
+     */
+    public void setCode(int code) {
+        this.code = code;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    //==========================================================================
+    /**
+     * getter.
+     * @return message
+     */
+    public String getMessage() {
+        return message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    //==========================================================================
+    /**
+     * setter.
+     * @param message String
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }    
+
+    //==========================================================================
+    /**
+     * getter.
+     * @return timestamp
+     */
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    //==========================================================================
+    /**
+     * setter.
+     * @param timestamp long
+     */
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    //==========================================================================
+    /**
+     * getter.
+     * @return date 
+     */
+    public String getDate() {
+        return date;
+    }
+
+    //==========================================================================
+    /**
+     * setter.
+     * @param date String
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
 
 }
